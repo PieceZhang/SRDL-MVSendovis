f31d530a3b63fb5608d6ebe411ab576fbe915d15
YuelinZhang <2239436414@qq.com>
2022-09-18 17:53:33 +0800

    add batch gray

---
+++ exe_batchgray.py
@@ -0,0 +1,23 @@
+# https://blog.csdn.net/qq_56839117/article/details/125216637
+import cv2
+import os
+
+img_list = []
+input_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam2"  # 要处理的图片所在的文件夹
+output_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam2_gray"  # 处理完的图片放在这里
+for item in os.listdir(input_path):
+    img_list.append(os.path.join(input_path, item))
+print(list)
+count = 1
+for imagepath in img_list:
+    # print(imagepath)
+    image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+    print(output_path + '%d.jpg' % count)  # 显示保存文件的路径及保存的文件名
+    cv2.imwrite(output_path + '%d.jpg' % count, image)
+    # 按一定路径将图片保存下来并命名，加号左边代表保存路径，右边代表文件命
+    # %d代表后面的% count中的count的数值
+    print("-----------执行中，保存第{}张----------".format(count + 1))
+    count += 1
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    cv2.imshow('garyimg', image)
+    cv2.waitKey(0)
