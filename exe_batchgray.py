50bebc1887b15d308e9719df2c3c0707d1b32906
YuelinZhang <2239436414@qq.com>
2022-09-19 10:53:33 +0800

    fix bug in batch gray

--- exe_batchgray.py
+++ exe_batchgray.py
@@ -3,8 +3,9 @@ import cv2
 import os
 
 img_list = []
-input_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam2"  # 要处理的图片所在的文件夹
-output_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam2_gray"  # 处理完的图片放在这里
+input_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam1"  # 要处理的图片所在的文件夹
+output_path = r"D:\B_SRDL\SRDL-MVSendovis\imgcap\cam1_gray"  # 处理完的图片放在这里
+ouput_name = 'cam1'
 for item in os.listdir(input_path):
     img_list.append(os.path.join(input_path, item))
 print(list)
@@ -12,8 +13,8 @@ count = 1
 for imagepath in img_list:
     # print(imagepath)
     image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-    print(output_path + '%d.jpg' % count)  # 显示保存文件的路径及保存的文件名
-    cv2.imwrite(output_path + '%d.jpg' % count, image)
+    print(output_path + '\\' + '{}_{}.jpg'.format(count, ouput_name))  # 显示保存文件的路径及保存的文件名
+    cv2.imwrite(output_path + '\\' + '{}_{}.jpg'.format(count, ouput_name), image)
     # 按一定路径将图片保存下来并命名，加号左边代表保存路径，右边代表文件命
     # %d代表后面的% count中的count的数值
     print("-----------执行中，保存第{}张----------".format(count + 1))
